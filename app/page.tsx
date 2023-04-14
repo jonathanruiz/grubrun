b5568e9a64cc8f4ea81271ed5da27f3778bc1313
Johnny Ruiz <johnny@ruizops.com>
2023-04-13 21:47:22 -0600

    changed heading tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl">Hello World!</h1>
+      <h2 className="text-4xl">Hello World!</h2>
     </main>
   );
 };
