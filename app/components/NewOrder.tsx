2a4cfd0f4d0e8b1a8f62b34c155b7797eb24b18d
Johnny Ruiz <johnny@ruizops.com>
2023-04-17 19:01:50 -0600

    fixed styling

--- app/components/NewOrder.tsx
+++ app/components/NewOrder.tsx
@@ -16,36 +16,36 @@ const NewOrder = () => {
 
   return (
     <form onSubmit={handleSubmit(createOrder)} className="w-1/3">
-      <div>
+      <div className="mb-5">
         <label htmlFor="name">Name</label>
         <input
           {...(register("name"), { required: true })}
           placeholder="Full Name"
-          className="w-full block"
+          className="w-full block h-10 bg-neutral-900"
         ></input>
       </div>
-      <div>
+      <div className="mb-5">
         <label htmlFor="email">Email</label>
         <input
           {...(register("email"), { required: true })}
           placeholder="Email"
-          className="w-full block"
+          className="w-full block h-10 bg-neutral-900"
         ></input>
       </div>
-      <div>
+      <div className="mb-5">
         <label htmlFor="maxOrderSize">Max Order Size</label>
         <input
           {...register("maxOrderSize")}
           placeholder="Max # of Orders"
-          className="w-full block"
+          className="w-full block h-10 bg-neutral-900"
         ></input>
       </div>
-      <div>
+      <div className="mb-5">
         <label htmlFor="departureTime">Departure Time</label>
         <input
           {...register("departureTime")}
           placeholder="What time are you leaving?"
-          className="w-full block"
+          className="w-full block h-10 bg-neutral-900"
         ></input>
       </div>
       <button type="submit" className="bg-blue-800 p-4 rounded">
