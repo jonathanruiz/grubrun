5a55f9ea906c59b4167c296a37e0002e2774aa02
Johnny Ruiz <johnny@ruizops.com>
2023-06-02 22:21:19 -0600

    Refactored to use shadcnUI (#4)
    * added shadcn ui
    
    * removed dark styling
    
    * added new shadui
    
    * formatted the document
    
    * changed github link
    
    * added form and label
    
    * added new form via react hook form
    
    * removed Next.js icon
    
    * refactored tailwind.config.js

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+    name: "Grubrun",
+    description:
+        "Create a GrubRun and invite your friends to join you for a meal!",
+    mainNav: [
+        {
+            title: "Home",
+            href: "/",
+        },
+    ],
+    links: {
+        github: "https://github.com/jonathanruiz/grubrun/",
+    },
+}
