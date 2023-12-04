5b4cf3b48e5e4d82ac4a0dc45b9ba25a349cebe3
Johnny Ruiz <johnny@ruizops.com>
2023-12-04 11:50:39 -0700

    created websocket client

--- client/src/components/OrderForm.tsx
+++ client/src/components/OrderForm.tsx
@@ -1,6 +1,46 @@
+import { useState, useEffect } from "react";
+
 const OrderForm = () => {
+  const [ws, setWs] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    // Create a WebSocket connection to the server
+    const websocket = new WebSocket("ws://localhost:8000/ws");
+
+    // Listen for messages from the server
+    websocket.onopen = () => {
+      console.log("WebSocket connection opened");
+    };
+
+    websocket.onmessage = (event) => {
+      console.log(`Received: ${event.data}`);
+    };
+
+    websocket.onerror = (error) => {
+      console.log(`WebSocket error: ${error}`);
+    };
+
+    websocket.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+
+    setWs(websocket);
+
+    return () => {
+      websocket.close();
+    };
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    if (ws) {
+      ws.send(JSON.stringify(Object.fromEntries(data)));
+    }
+  };
+
   return (
-    <form className="flex flex-col space-y-4">
+    <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
       <input
         className="border p-2"
