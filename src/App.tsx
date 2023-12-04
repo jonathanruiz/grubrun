e76502b3271af003067ed061258f337c71e454fd
Johnny Ruiz <johnny@ruizops.com>
2023-12-04 09:53:36 -0700

    styled application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import OrderForm from "./components/OrderForm";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 text-left">
+        <h1 className="text-3xl font-bold underline">Grub Run</h1>
+        <OrderForm />
+      </div>
+    </div>
   );
 }
 
