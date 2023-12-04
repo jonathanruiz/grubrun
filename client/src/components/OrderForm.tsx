8c9c9c86db002a896325a1a740578253171ad9a2
Johnny Ruiz <johnny@ruizops.com>
2023-12-04 11:45:23 -0700

    update form

--- client/src/components/OrderForm.tsx
+++ client/src/components/OrderForm.tsx
@@ -2,18 +2,28 @@ const OrderForm = () => {
   return (
     <form className="flex flex-col space-y-4">
       <label htmlFor="name">Name</label>
-      <input className="border p-2" type="text" placeholder="Name" />
+      <input
+        className="border p-2"
+        type="text"
+        placeholder="Enter your name here"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="border p-2"
+        type="text"
+        placeholder="Enter your email here"
+      />
+      <label htmlFor="max">Maximum Order Size</label>
+      <input
+        className="border p-2"
+        type="text"
+        placeholder="Enter maximum order size"
+      />
       <label htmlFor="time">Time until leaving (in minutes)</label>
       <input
         className="border p-2"
         type="text"
-        placeholder="Time until leaving (in minutes)"
-      />
-      <label htmlFor="max">Maximum number of orders</label>
-      <input
-        className="border p-2"
-        type="text"
-        placeholder="Maximum number of orders"
+        placeholder="Enter minutes until leaving"
       />
       <button
         className="bg-blue-500 text-white self-start py-2 px-4 rounded"
