ec2d934eeb9126efb0f4d23a5468fcfe52f24d80
Johnny Ruiz <johnny@ruizops.com>
2023-12-04 13:27:16 -0700

    fixed templating for route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,14 @@ import OrderForm from "./components/OrderForm";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-              <div className="max-w-md w-full space-y-8 text-left">
-                <h1 className="text-3xl font-bold underline">Grub Run</h1>
-                <OrderForm />
-              </div>
-            </div>
-          }
-        />
-      </Routes>
+      <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8 text-left">
+          <h1 className="text-3xl font-bold underline">Grub Run</h1>
+          <Routes>
+            <Route path="/" element={<OrderForm />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
