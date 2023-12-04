14fe0cdc47d3ba630213f69ebd2b9472ea683ec1
Johnny Ruiz <johnny@ruizops.com>
2023-12-04 13:05:45 -0700

    navigate to new page when handleSubmit is done

--- client/src/components/OrderForm.tsx
+++ client/src/components/OrderForm.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const OrderForm = () => {
+  const navigate = useNavigate();
   const [ws, setWs] = useState<WebSocket | null>(null);
 
   useEffect(() => {
@@ -37,6 +39,8 @@ const OrderForm = () => {
     if (ws) {
       ws.send(JSON.stringify(Object.fromEntries(data)));
     }
+
+    navigate("/order");
   };
 
   return (
