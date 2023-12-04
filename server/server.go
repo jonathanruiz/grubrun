efb9cc7d871700e322c9331cb87312854f073a3d
Johnny Ruiz <johnny@ruizops.com>
2023-12-04 11:37:55 -0700

    Created Go Backend (#2)
    * moved react to client
    
    * created server
    
    * updateto have it listen and serve
    
    * added log for successful connection

---
+++ server/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// Takes a normal HTTP connection and upgrades it to a WebSocket connection.
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Upgrade initial GET request to a WebSocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("New WebSocket connection established")
+
+	// Close the connection when the function returns
+	defer ws.Close()
+
+	for {
+		// Read in a new message as JSON and map it to a Message object
+		messageType, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		log.Printf("Received a %v message: %s", messageType, message)
+	}
+}
+
+func main() {
+	// Configure websocket route
+	http.HandleFunc("/ws", handleConnections)
+
+	// Start the server on localhost port 8000 and log any errors
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
