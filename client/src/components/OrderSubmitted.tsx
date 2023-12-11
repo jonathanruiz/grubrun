7bf33d861d4d9d34a95bba037a240f9aa93c1d7f
Johnny Ruiz <johnny@ruizops.com>
2023-12-11 10:28:09 -0700

    added API post request to client

---
+++ client/src/components/OrderSubmitted.tsx
@@ -0,0 +1,12 @@
+const OrderSubmitted = () => {
+  return (
+    <div>
+      <h2>Order Submitted</h2>
+      <p>Thank you for your order!</p>
+      <p>Here is the link to your order run page: </p>
+      <p>LINK</p>
+    </div>
+  );
+};
+
+export default OrderSubmitted;
