ec02709c1b0a9ef4025de0c6e9d3a97033c37a3d
Johnny Ruiz <johnny@ruizops.com>
2024-02-06 23:23:03 -0700

    Limit orders by max number of orders (#11)
    * check if max was exceeded
    
    * disabled form when max is exceeded
    
    * hides form immediately if max has been reached

--- client/src/components/OrderCollect.tsx
+++ client/src/components/OrderCollect.tsx
@@ -21,8 +21,28 @@ const OrderCollect = () => {
   const { orderId } = useParams<{ orderId: string }>();
   const [ws, setWS] = useState<WebSocket | null>(null);
   const [orderRun, setOrderRun] = useState<OrderRun | null>(null);
+  const [maxReached, setMaxReached] = useState(false);
 
   console.log("Here is the order: ", orderRun);
+
+  fetch(`${API_BASE_URL}/api/getOrderRun?orderId=${orderId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data: OrderRun) => {
+      // @ts-expect-error - data is not null
+      if (data[orderId].orders.length >= parseInt(data[orderId].max)) {
+        setMaxReached(true);
+      }
+    })
+    .catch((err) => {
+      console.error("Error was thrown:", err);
+    });
 
   useEffect(() => {
     const websocket = new WebSocket("ws://localhost:8000/ws");
@@ -86,35 +106,63 @@ const OrderCollect = () => {
     // Add the orderId to the JSON data
     jsonData.orderId = orderId || "";
 
+    // Check if orderId is defined
+    if (orderId !== undefined) {
+      // @ts-expect-error - Get the current OrderRun
+      const currentOrderRun = orderRun[orderId];
+
+      // Check if the currentOrderRun and its orders property are defined
+      if (
+        currentOrderRun &&
+        currentOrderRun.orders &&
+        currentOrderRun.orders.length >= parseInt(currentOrderRun.max)
+      ) {
+        alert("Maximum number of orders reached for this run.");
+        setMaxReached(true);
+        return;
+      }
+    } else {
+      alert("Order ID is not defined.");
+      return;
+    }
+
     if (ws) {
       ws.send(JSON.stringify(jsonData));
     }
   };
   return (
     <>
-      <h2>Place your order here</h2>
-      <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          className="border p-2"
-          name="name"
-          type="text"
-          placeholder="Enter your name here"
-        />
-        <label htmlFor="order">Order</label>
-        <input
-          className="border p-2"
-          name="order"
-          type="text"
-          placeholder="Enter your order here"
-        />
-        <button
-          className="bg-blue-500 text-white self-start py-2 px-4 rounded"
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
+      {maxReached ? (
+        <h2 className="text-2xl font-bold">
+          Maximum number of orders reached for this run.
+        </h2>
+      ) : (
+        <>
+          <h2 className="text-2xl font-bold">Place your order here</h2>
+          <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+              className="border p-2"
+              name="name"
+              type="text"
+              placeholder="Enter your name here"
+            />
+            <label htmlFor="order">Order</label>
+            <input
+              className="border p-2"
+              name="order"
+              type="text"
+              placeholder="Enter your order here"
+            />
+            <button
+              className="bg-blue-500 text-white self-start py-2 px-4 rounded"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </>
+      )}
 
       <div className="pt-6 pb-8 mb-4 flex flex-col my-2">
         <h3 className="text-gray-500 text-md uppercase font-bold tracking-wider">
