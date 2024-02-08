782052e5123d16377dc76f72f01291ebe037cb0f
Johnny Ruiz <johnny@ruizops.com>
2024-02-07 22:29:13 -0700

    added react hook form and zod for OrderRunForm

--- client/src/components/OrderRunForm.tsx
+++ client/src/components/OrderRunForm.tsx
@@ -1,29 +1,44 @@
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
 import config from "../../config";
+
+const schema = zod.object({
+  name: zod.string(),
+  email: zod.string().email(),
+  location: zod.string(),
+  max: zod.number(),
+  time: zod.number(),
+});
 
 const API_BASE_URL = config.api.baseUrl;
 
 const OrderRunForm = () => {
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const data = new FormData(e.currentTarget);
-
+  // @ts-expect-error - data is not null
+  const submitForm = async (data) => {
     // Create a JavaScript object with the form data
-    const jsonData = Object.fromEntries(data.entries());
+    // const jsonData = Object.fromEntries(data.entries());
 
-    // @ts-expect-error - Convert the time and max properties to numbers
-    jsonData.time = Number(jsonData.time);
-    // @ts-expect-error - Convert the time and max properties to numbers
-    jsonData.max = Number(jsonData.max);
+    console.log(data);
+    // console.log(jsonData);
 
     const response = await fetch(`${API_BASE_URL}/api/createOrder`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(jsonData), // Convert the JavaScript object to a JSON string
+      body: JSON.stringify(data), // Convert the JavaScript object to a JSON string
     })
       .then((res) => res.json())
       .catch((err) => {
@@ -34,7 +49,10 @@ const OrderRunForm = () => {
   };
 
   return (
-    <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+    <form
+      className="flex flex-col space-y-4"
+      onSubmit={handleSubmit(submitForm)}
+    >
       <h2 className="text-2xl font-bold">Order Run Form</h2>
       <label
         className="text-gray-500 text-sm uppercase font-bold tracking-wider"
@@ -44,10 +62,13 @@ const OrderRunForm = () => {
       </label>
       <input
         className="border p-2"
-        name="name"
         type="text"
         placeholder="Enter your name here"
+        {...register("name")}
       />
+      {/* @ts-expect-error - error */}
+      {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+
       <label
         className="text-gray-500 text-sm uppercase font-bold tracking-wider"
         htmlFor="email"
@@ -56,10 +77,13 @@ const OrderRunForm = () => {
       </label>
       <input
         className="border p-2"
-        name="email"
         type="text"
         placeholder="Enter your email here"
+        {...register("email")}
       />
+      {/* @ts-expect-error - error */}
+      {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+
       <label
         className="text-gray-500 text-sm uppercase font-bold tracking-wider"
         htmlFor="locaiton"
@@ -68,10 +92,15 @@ const OrderRunForm = () => {
       </label>
       <input
         className="border p-2"
-        name="location"
         type="text"
         placeholder="Enter location here"
+        {...register("location")}
       />
+      {errors.location && (
+        // @ts-expect-error - error
+        <p className="text-red-500">{errors.location.message}</p>
+      )}
+
       <label
         className="text-gray-500 text-sm uppercase font-bold tracking-wider"
         htmlFor="max"
@@ -80,10 +109,13 @@ const OrderRunForm = () => {
       </label>
       <input
         className="border p-2"
-        name="max"
         type="number"
         placeholder="Enter maximum order size"
+        {...register("max", { valueAsNumber: true })}
       />
+      {/* @ts-expect-error - error */}
+      {errors.max && <p className="text-red-500">{errors.max.message}</p>}
+
       <label
         className="text-gray-500 text-sm uppercase font-bold tracking-wider"
         htmlFor="time"
@@ -92,10 +124,13 @@ const OrderRunForm = () => {
       </label>
       <input
         className="border p-2"
-        name="time"
         type="number"
         placeholder="Enter minutes until leaving"
+        {...register("time", { valueAsNumber: true })}
       />
+      {/* @ts-expect-error - error */}
+      {errors.time && <p className="text-red-500">{errors.time.message}</p>}
+
       <button
         className="bg-blue-500 text-white self-start py-2 px-4 rounded"
         type="submit"
